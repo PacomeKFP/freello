bd0e4e49820f5afc10258582e23b65a4df62a7dc
Duroy51 <131318577+Duroy51@users.noreply.github.com>
2024-04-14 17:48:53 +0100

    Add files via upload

---
+++ freello/src/main/java/org/enspy/freello/freello/FreelloApplication.java
@@ -0,0 +1,16 @@
+package org.enspy.freello.freello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelloApplication.class, args);
+	}
+
+}
+
+
+
---
+++ freello/target/classes/freello.sql
@@ -0,0 +1,40 @@
+
+
+CREATE TABLE IF NOT EXISTS task (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  title VARCHAR(250) NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  status VARCHAR(250) NOT NULL,
+  date DATE
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(250) NOT NULL,
+  email VARCHAR(250) NOT NULL,
+  password VARCHAR(250) NOT NULL,
+  
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(250) NOT NULL,
+  admin_id INT NOT NULL,
+  FOREIGN KEY (admin_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS project_members (
+  project_id INT NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (project_id, user_id),
+  FOREIGN KEY (project_id) REFERENCES projects(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS task_workers (
+  task_id INT NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (task_id, user_id),
+  FOREIGN KEY (task_id) REFERENCES task(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
