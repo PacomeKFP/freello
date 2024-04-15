694e88ddd847fffc2f57d73b802b475878cd4b77
Ngo-Bassom-Anne-Rosa <151435188+Ngo-Bassom-Anne-Rosa@users.noreply.github.com>
2024-04-15 09:00:19 +0100

    Update Task.java

--- src/main/java/org/enspy/freello/model/Task.java
+++ src/main/java/org/enspy/freello/model/Task.java
@@ -1,22 +1,50 @@
 
 package org.enspy.freello.model;
 
+import java.util.List;
 
+import org.apache.catalina.User;
+import org.enspy.freello.Users;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Task")
 public class Task {
 
-    private long taskid;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    private long taskid;
     private String name;
     private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    public enum Status{
+        Pending,OnGoing, Completed
+    }
+
+    private List<Users> workers;
 
     public Task() {
     }
 
-    public Task(String name, String description) {
+    public Task(long taskid, String name, String description, Status status, List<Users> workers) {
+        this.taskid = taskid;
         this.name = name;
         this.description = description;
+        this.status = status;
+        this.workers = workers;
     }
 
+   
     
 
 }
