987a380a7c5c5a63e8d24715b684ae71db0723b3
Ngo-Bassom-Anne-Rosa <151435188+Ngo-Bassom-Anne-Rosa@users.noreply.github.com>
2024-04-15 09:28:02 +0100

    Update ProjectModel.java

--- src/main/java/org/enspy/freello/model/ProjectModel.java
+++ src/main/java/org/enspy/freello/model/ProjectModel.java
@@ -1,22 +1,33 @@
 package org.enspy.freello.model;
 
-import org.enspy.freello.Users;
-import org.enspy.freello.Projects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 
+@Entity
+@Table(name="ProjectModel")
 public class ProjectModel {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long projectID;
     private String name;
-    private Users admin;
-    private Users members[];
 
+    
 
-    public ProjectModel(long projectID, String name, Users admin, Users[] members) {
-        this.projectID = projectID;
-        this.name = name;
-        this.admin = admin;
-        this.members = members;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    public enum Status{
+        Pending,OnGoing, Completed
     }
+
+    
+
+
+    
 
 
 
